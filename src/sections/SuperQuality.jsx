31fcb1866ab44c719c93183c7e6726147460f4d6
Fahad Fateh <fahadfateh1000@gmail.com>
2024-11-29 11:28:48 -0800

    finish working on the tilte, info text and button

--- src/sections/SuperQuality.jsx
+++ src/sections/SuperQuality.jsx
@@ -7,20 +7,24 @@ const SuperQuality = () => {
 			className="flex justify-between items-center max-lg:flex-col gap-10 w-full max-container"
 		>
 			<div className="flex flex-1 flex-col">
-				<h2 className="mt-10 font-palanquin text-4xl capitalize font-bold">
-					<span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
-						The New Arrival
-					</span>
-					<br /> <span className="text-coral-red inline-block mt-3">Nike</span>{" "}
+				<h2 className="mt-10 font-palanquin text-4xl capitalize font-bold lg:max-w-lg">
+					We Provide You{" "}
+					<span className="text-coral-red inline-block mt-3">Super</span>
+					<span className="text-coral-red inline-block mt-3">Quality</span>{" "}
 					Shoes
 				</h2>
-				<p className="font-montserrat text-slate-gray mt-6 mb-14 sm:max-w-sm">
-					Discover stylish Nike arrivals, qulaity comfort, and innovation for
-					your active life
+				<p className="mt-4 lg:max-w-lg info-text">
+					Ensuring premium comfort and style , our meticiusly crafted footwear
+					is designed to elevate your experience with providing you with
+					unmatched quality, innovation, and a touch of elegance.
 				</p>
-				<Button label="View details" />
+				<p className="mt-6 lg:max-w-lg info-text">
+					Our dedication to detail and more excellence ensures your satisfaction
+				</p>
+				<div className="mt-11">
+					<Button label="View details" />
+				</div>
 			</div>
-			SuperQuality
 		</section>
 	);
 };
