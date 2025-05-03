2bb8d7384e74d686b0f2b3fad372d100bb899309
Fahad <fahadfateh1000@gmail.com>
2025-05-02 17:49:45 -0800

    Deploy to GitHub Pages

---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import CustomerReviews from "./CustomerReviews";
+
+export {
+	Hero,
+	Services,
+	Footer,
+	PopularProducts,
+	SpecialOffer,
+	SuperQuality,
+	Subscribe,
+	CustomerReviews,
+};
